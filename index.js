54a4b9867f3c484ba52f4e1d6b53be51a9da02de
Antti Kantola <antti.kantola@helsinki.fi>
2022-02-18 11:38:26 +0200

    Osa 3.7: Morgan added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {
     id: 1,
-    name: "Arto Hellas",
+    name: "Arto Hellas Muuutos",
     number: "040-1234567"
   },
   {
