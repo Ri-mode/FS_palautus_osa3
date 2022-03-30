6c2e5d7602b0bc6e5044e333421f91c3154379ea
Antti Kantola <antti.kantola@helsinki.fi>
2022-03-30 19:05:17 +0300

    Osa 3.8: Morgan configured

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const { request } = require('express')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+//app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('build'))
+
+morgan.token('bodyText', function(req, res) {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  } else {
+    return ''
+  }
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bodyText'))
+
+
+
 
 let persons = [
   {
